8adbdc9416a8cc280edd90ceec9a77364fd31e18
Artem Rudenko <fighterartem@gmail.com>
2024-01-15 09:19:29 +0200

    Merge step2. Add Description, ManufacturerCode and Price property to catalogueProduct and UpdateProductRequest model. Create a new migration AddProductDescriptionManCodePrice. Add new rules to UpdateProductRequestValidator.

--- KatlaSport.Services/ProductManagement/ProductManagementMappingProfile.cs
+++ KatlaSport.Services/ProductManagement/ProductManagementMappingProfile.cs
@@ -13,8 +13,10 @@ namespace KatlaSport.Services.ProductManagement
             CreateMap<DataAccessProductCategory, ProductCategoryListItem>();
             CreateMap<DataAccessProduct, ProductCategoryProductListItem>();
 
-            // TODO STEP 2 - Change the mapping below.
-            CreateMap<DataAccessProduct, Product>();
+            // TODO STEP 2 - Change the mapping below. - COMPLETED
+            CreateMap<DataAccessProduct, Product>()
+                   .ForMember(li => li.Description, opt => opt.MapFrom(p => p.Description == null ? string.Empty : p.Description))
+                   .ForMember(li => li.ManufacturerCode, opt => opt.MapFrom(p => p.ManufacturerCode == null ? string.Empty : p.ManufacturerCode));
 
             CreateMap<DataAccessProduct, ProductListItem>()
                 .ForMember(li => li.CategoryCode, opt => opt.MapFrom(p => p.Category.Code));
